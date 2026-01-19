b7317340ee0e480acf29756b68ba74429a49d14d
Ryosuke Takahashi <r-takahashi@image-pit.com>
2026-01-19 15:11:18 +0900

    Remove dist folder from tracking

--- shared/fumadocs-engine/dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from './site/spec.js';
-export * from './theme/derive.js';
-export * from './source/fumadocs-source.js';
-export * from './navigation/sidebar.js';
-export * from './mdx/mdx-options.js';
--- shared/fumadocs-engine/dist/shims.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'fumadocs-core'
-declare module 'fumadocs-core/*'
-declare module 'fumadocs-core/server'
-declare module 'fumadocs-core/source'
-declare module 'fumadocs-core/mdx-plugins'
-
-declare module 'fumadocs-mdx'
-declare module 'fumadocs-mdx/*'
-declare module 'fumadocs-mdx/config'
